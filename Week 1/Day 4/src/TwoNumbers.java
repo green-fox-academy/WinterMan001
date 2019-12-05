8cbd19127d6c1df5c612c504aef1dce5a3a4d3c4
Kristof <hq1szkgmail.com>
2019-12-05 13:37:03 +0100

    ok

---
+++ Week 1/Day 4/src/TwoNumbers.java
@@ -0,0 +1,20 @@
+public class TwoNumbers {
+    public static void main (String[]args){
+
+        int a = 13;
+        int b = 22;
+
+        System.out.println(13 + 22);
+        System.out.println(22 - 13);
+        System.out.println(22 * 13);
+        System.out.println(22 / 13);
+
+        System.out.println(a / b);
+
+        int remainder =  a % 1;
+        System.out.println("Remainder = " + remainder);
+
+
+
+    }
+}
