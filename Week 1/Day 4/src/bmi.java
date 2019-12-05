b6dbacaf21a9989484b62990ef62437af39c81f9
Kristof <hq1szkgmail.com>
2019-12-05 14:37:57 +0100

    oooookay

---
+++ Week 1/Day 4/src/bmi.java
@@ -0,0 +1,10 @@
+public class bmi {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        double BMI = (massInKg / heightInM);
+        System.out.println("BMI is: " + BMI);
+        // Print the Body mass index (BMI) based on these values
+    }
+}
