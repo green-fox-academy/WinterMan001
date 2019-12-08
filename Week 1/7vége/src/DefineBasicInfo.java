a62d90701ee6c6eb8463a7c588b6568fca2f79c5
Kristof <hq1szkgmail.com>
2019-12-08 18:01:25 +0100

    DefineBasicinfo

---
+++ Week 1/7vége/src/DefineBasicInfo.java
@@ -0,0 +1,18 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+
+        String myName = "Kristof";
+        int myAge = 29;
+        double myHeight = 1.7;
+        boolean married = false;
+
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println("Am I married?" + married);
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+    }
+}
