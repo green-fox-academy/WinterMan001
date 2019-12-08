308e3e1b1cf09febe905e6b97be28e8990763da5
Kristof <hq1szkgmail.com>
2019-12-08 17:54:32 +0100

    helllo

---
+++ Week 1/7vége/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[]args){
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hey Dave!");
+        System.out.println("Hey Johny!");
+        System.out.println("Hey Pityu!");
+    }
+}
