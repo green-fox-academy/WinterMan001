e9dd72e04a9878a0226ea64fcc110d3533060363
Kristof <hq1szkgmail.com>
2019-12-09 13:26:04 +0100

    FirstLast

---
+++ Week 2/src/SumOfElements.java
@@ -0,0 +1,13 @@
+public class SumOfElements {
+    public static void main(String[]Args){
+        // - Create an array variable named `r`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+        int r [] = {54,23,66,12};
+        int firstItem = r[0];
+        int lastItem = r[3];
+        System.out.println(firstItem + lastItem);
+
+
+    }
+}
