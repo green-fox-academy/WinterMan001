0c1b8eb454d18489851c2452259858ead7bac9b4
Kristof <hq1szkgmail.com>
2019-12-10 16:51:20 +0100

    Personal finance

---
+++ Week 2/Day 2/src/MapIntroduction2.java
@@ -0,0 +1,24 @@
+/*import java.util.HashMap;
+
+public class MapIntroduction2 {
+    public static void main(String[]Args){
+
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("978-1-60309-452-8", "A letter to Jo");
+        map.put("978-1-60309-459-7", "Lupus");
+        map.put("978-1-60309-444-3,", "Red panda and moon bear");
+        map.put("978-1-60309-461-0 ", "The Lab");
+
+        for (String map : map.keySet()){
+            System.out.println(Key + "" + map.entrySet())
+
+
+        }
+
+        for (map.Entry<String, String>) entry : map.entrySet()){
+
+        }
+
+    }
+}
+*/
---
+++ Week 2/Day 2/src/PersonalFinance.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PersonalFinance {
+    public static void main(String[]Args){
+        ArrayList<Integer> expenses = new ArrayList<Integer>();
+        expenses.add(500);
+        expenses.add(1000);
+        expenses.add(1250);
+        expenses.add(175);
+        expenses.add(800);
+        expenses.add(120);
+        System.out.println(expenses);
+
+        double sum = 0;
+        for (double i : expenses)
+            sum += i;
+        System.out.println(sum);
+
+        System.out.println(Collections.max(expenses));
+        System.out.println(Collections.min(expenses));
+
+
+
+            double average = sum / expenses.size();
+            System.out.println("The average is : " + average);
+        
+
+
+
+    }
+}
